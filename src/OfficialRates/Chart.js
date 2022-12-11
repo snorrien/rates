9454429e83de8ee76ad6ffdabee25597017efd9b
Alena Shapovalova <avshapovalova31@gmail.com>
2022-12-11 23:35:06 +0100

    fix charts

--- src/OfficialRates/Chart.js
+++ src/OfficialRates/Chart.js
@@ -1,14 +1,12 @@
 import React from "react";
-import GetCharts from  "../Services/ChartsApi";
-
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, } from 'recharts';
 
 function Chart(props) {
     return (
         <div>
             <LineChart width={600} height={300} data={props.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                <Line dataKey="eur" type="monotone" stroke="#8884d8" />
-                <Line dataKey="usd" type="monotone" stroke="#82ca9d" />
+                <Line dataKey="EUR" type="monotone" stroke="#8884d8" />
+                <Line dataKey="USD" type="monotone" stroke="#82ca9d" />
 
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="date" />
@@ -17,7 +15,6 @@ function Chart(props) {
             </LineChart>
         </div>
     )
-    
 }
 
 export default Chart;
