2a0f7766fc6524a8efc21a717617e3cd2cf951ce
Oleg Shakhmatov <oleg.shakhmatov@billetkontoret.dk>
2022-12-15 20:05:00 +0100

    add docker

--- src/Services/HttpGet.js
+++ src/Services/HttpGet.js
@@ -3,13 +3,12 @@ import { useEffect, useState } from 'react';
 function HttpGet(url) {
   const [result, setResult] = useState([]);
 
-  useEffect(() => {;
-    fetch(`http://localhost:5276/${url}`) // 5276 backend
-      .then((res) => res.json())
-     
-      .then((res) => {
-        setResult(res);
-      });
+  useEffect(() => {
+      fetch(`http://158.160.36.14:81/${url}`) // 5276 backend
+          .then((res) => res.json())
+          .then((res) => {
+            setResult(res);
+          });
   }, []);
 
   return result;
