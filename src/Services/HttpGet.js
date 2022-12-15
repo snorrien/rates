f74f8334071d43a90ccb256111be4d17e435faad
Alena Shapovalova <avshapovalova31@gmail.com>
2022-12-15 20:01:24 +0100

    wip

--- src/Services/HttpGet.js
+++ src/Services/HttpGet.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
 function HttpGet(url) {
-  let [result, setResult] = useState([]);
+  const [result, setResult] = useState([]);
 
-  useEffect(() => {
-      fetch(`http://localhost:5276/${url}`) // 5276 backend
-          .then((res) => res.json())
-          .then((res) => {
-            setResult(res);
-          });
+  useEffect(() => {;
+    fetch(`http://localhost:5276/${url}`) // 5276 backend
+      .then((res) => res.json())
+     
+      .then((res) => {
+        setResult(res);
+      });
   }, []);
 
-  console.log(result);
   return result;
 }
 
